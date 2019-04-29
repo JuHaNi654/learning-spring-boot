ad32ed8f83b13f4d6a5a872df143a7f92e74b9fd
Juhani <>
2019-04-29 19:27:16 +0300

    fixed code

--- src/main/java/fi/resthubservice/rest/RestApplication.java
+++ src/main/java/fi/resthubservice/rest/RestApplication.java
@@ -30,23 +30,23 @@ public class RestApplication {
 		SpringApplication.run(RestApplication.class, args);
 
 	}
-	
+	//Saving example data
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-			
+			//Saving users to the database
 			User user = new User("rolli", "$2a$04$IEbRJGn1avY3xT7mCB2Qkusm2CN6RnPWGX6lXqD923MlTRR3Vuq2u", "OWNER", "testi@testi.fi");
 			repository.save(user);
 			User user2 = new User("user", "$2a$06$7U.M8ba1Hpg/41OiapCTquxJv5A7vsxSUhSRy33wlgqGOjoHJE8ca", "USER", "user@testi.fi");
 			repository.save(user2);
 			
-			
+			// Saving hubs to the database
 			CommunityHub hub = new CommunityHub("TestingHub",  new Date(), "no");
 			chrepository.save(hub);
 			CommunityHub hub2 = new CommunityHub("UserHub", new Date(), "no");
 			chrepository.save(hub2);
 			
-			
+			// Saving hub members to the database
 			Member member = new Member(new JoinedMemberKey(user.getId(), hub.getChId()), user, hub, "admin");
 			mrepository.save(member);
 			Member member2 = new Member(new JoinedMemberKey(user2.getId(), hub2.getChId()), user2, hub2, "admin");
